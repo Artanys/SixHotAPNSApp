ec83c8b9ad24e3b3d4b8c1859e4d3168397f2ecd
Jordan Naumann <jordan.naumann@gmail.com>
2018-04-18 00:55:11 -0400

    Initial Commit

---
+++ SixHotAPNSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SixHotAPNSApp
+//
+//  Created by Jordan Naumann on 4/18/18.
+//  Copyright © 2018 SixHotAPNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SixHotAPNSApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SixHotAPNSApp
+//
+//  Created by Jordan Naumann on 4/18/18.
+//  Copyright © 2018 SixHotAPNS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
